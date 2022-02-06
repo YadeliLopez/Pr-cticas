e28f0d81796de081dc93ae9c07d510062e3dac68
Yadeli Lopez <ingrid_yadeli@hotmail.com>
2022-02-05 22:46:10 -0600

    Actualización

--- MiniProyect/toDo.js
+++ MiniProyect/toDo.js
@@ -65,6 +65,14 @@ const pintarTareas = () => {
         //cuando tenemos un template hay que hacer clon primero
         const clone = template.cloneNode(true);
         clone.querySelector("p").textContent = tarea.texto;
+
+        if(tarea.estado){
+            clone.querySelector("alert").classList.replace("alert-warning", "alert-primary");
+            clone.querySelector(".fas")[0].classList.replace("fa-check-circle", "fa-undo-alt");
+        }
+
+        clone.querySelectorAll(".fas")[0].dataset.id = tarea.id;
+        clone.querySelectorAll(".fas")[1].dataset.id = tarea.id;
         fragment.appendChild(clone);
     })
 
@@ -72,5 +80,17 @@ const pintarTareas = () => {
 }
 
 const btnAccion = e => {
-    
+    //console.log(e.target.classList.contains("fa-check-circle"));
+    if(e.target.classList.contains("fa-check-circle")){
+        console.log(e.target.dataset.id);
+        tareas[e.target.dataset.id].estado = true;
+        pintarTareas();
+        //console.log(tareas);
+    }
+
+    if(e.target.classList.contains("fa-minus-circle")){
+        delete tareas[e.target.dataset.id];
+    }
+
+    e.stopPropagation();
 }
